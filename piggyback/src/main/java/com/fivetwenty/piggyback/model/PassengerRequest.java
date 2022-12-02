e7d7abc9e9655d0212016cd477be3cb8d3927a98
Debabrata <debabrata.halder2014@gmail.com>
2022-12-02 11:44:34 -0500

    Revert "Time in Customer Request"
    This reverts commit 338445946cc6ad356fe415336c11d6cd0ab8f563.

--- piggyback/src/main/java/com/fivetwenty/piggyback/model/PassengerRequest.java
+++ piggyback/src/main/java/com/fivetwenty/piggyback/model/PassengerRequest.java
@@ -3,23 +3,11 @@ package com.fivetwenty.piggyback.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
-
 @Document("PassengerRequest")
 public class PassengerRequest extends CustomerRequest {
 
     @Id
     String id = null;
-
-    public LocalDateTime getLocalDateTime() {
-        return localDateTime;
-    }
-
-    public void setLocalDateTime(LocalDateTime localDateTime) {
-        this.localDateTime = localDateTime;
-    }
-
-    LocalDateTime localDateTime;
 
     public PassengerRequest(String userId, String src, String dst){
         this.setUserId(userId);
