99ff440e73cadad3443bfd1204fb75095c9c6b14
Piusha Gullapalli <piusha.gullapalli@gmail.com>
2022-12-07 22:11:01 -0500

    Browser routing

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -8,18 +8,31 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 // import here the component you want to render
 import Login from "./Login/Login.js";
 import RideBook from "./RideBook/RideBook.js"
-import RideMatch from "./RideMatch";
-import RideConfirm from "./RideConfirm";
+import RideMatch from "./RideMatch/RideMatch.js";
+import RideConfirm from "./RideConfirm/RideConfirm.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+//
+// ReactDOM.render(
+//     <BrowserRouter>
+//         <Routes>
+//             <Route exact path="/" component={<Login/>} />
+//             <Route path="/RideBook" component={<RideBook/>} />
+//             <Route path="/RideMatch" component={<RideMatch/>} />
+//             <Route path="/RideConfirm" component={<RideConfirm/>} />
+//         </Routes>
+//     </BrowserRouter>,
+//     root
+// );
 // Add here the name of the component you want to render - For Eg. RideBook
 root.render(
   <React.StrictMode>
-    <Login/><RideBook/><RideMatch/><RideConfirm/>
+    <Login/>
   </React.StrictMode>
 );
 
